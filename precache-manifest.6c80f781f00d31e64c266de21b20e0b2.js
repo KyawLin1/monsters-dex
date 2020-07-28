173a572361135739870aac2283f96c67ba508a0a
KyawLin1 <didikyawlinn@gmail.com>
2020-07-29 01:55:48 +0800

    Updates

---
+++ precache-manifest.6c80f781f00d31e64c266de21b20e0b2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71a14bed766c402584055414eab2fa41",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "fe54dfe516ac6a2eb04b",
+    "url": "/monsters-rolodex/static/css/main.7954f7e4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fe54dfe516ac6a2eb04b",
+    "url": "/monsters-rolodex/static/js/main.eb425bab.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
